61bee7c5b23746caf6b1db57e95c13cdef3f7433
igrok1803444 <128973938+igrok1803444@users.noreply.github.com>
2024-04-15 16:29:33 +0300

    Update UnloginMenu.styled.js add responsive design

--- src/components/unlogin-menu/UnloginMenu.styled.js
+++ src/components/unlogin-menu/UnloginMenu.styled.js
@@ -4,6 +4,11 @@ export const UnLoginMenu = styled.div`
   display: flex;
 
   column-gap: 8px;
+   @media screen and (max-width: 1023px) {
+    flex-wrap: wrap;
+    justify-content: center;
+    row-gap: 18px;
+  }
 `;
 
 export const AuthButtonText = styled.p`
